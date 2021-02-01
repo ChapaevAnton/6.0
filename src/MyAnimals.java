13821365959b819bfece5d612e461470808bea9a
Chapaev Anton <virsus2009@gmail.com>
2021-02-01 13:01:56 +0700

    up 6.4.6

--- src/MyAnimals.java
+++ src/MyAnimals.java
@@ -1,47 +1,83 @@
-public class MyAnimals{
+public class MyAnimals {
+
+    private Animal[] zoo;
+
+    public MyAnimals() {
+        zoo = new Animal[10];
+        for (int i = 0; i < zoo.length; i++) {
+            if (Math.random() < .5)
+                zoo[i] = new Cat();
+            else zoo[i] = new Dog();
+        }
+    }
+
+    public void print() {
+        for (Animal animal : zoo) {
+            animal.print();
+        }
+    }
+
+    public void replacementAnimal(Animal animal, int position) {
+        if (position < zoo.length && position >= 0) zoo[position] = animal;
+    }
+
     public static void main(String[] args) {
-        Animal animal = new Cat();
-        Animal animal1 = new Dog();
-        Animal animal2 = new Reptile();
-        animal.print();
-        animal1.print();
+
+        MyAnimals myZoo = new MyAnimals();
+        myZoo.print();
+
+        myZoo.replacementAnimal(new Reptile(),2);
+        myZoo.print();
+
+        myZoo.replacementAnimal(new Animal(),3);
+        myZoo.print();
+
+        Animal animal2 = new Dog();
+        Animal animal3 = new Reptile();
+
         animal2.print();
-        System.out.println(animal instanceof Cat);
-        System.out.println(animal instanceof Dog);
-        System.out.println(animal2 instanceof Mammal);
+        animal3.print();
+
+        System.out.println(animal2 instanceof Dog);
+        System.out.println(animal3 instanceof Mammal);
+
+
     }
 }
 
 class Animal {
+
     protected void print() {
         System.out.println("Животное");
     }
 }
 
 class Mammal extends Animal {
+
     @Override
     protected void print() {
         System.out.println("Млекопитающее");
     }
 }
 
-class Cat extends Mammal{
+class Cat extends Mammal {
+
     @Override
-    public void print(){
+    public void print() {
         System.out.println("Я кошка");
     }
 }
 
-class Dog extends Mammal{
+class Dog extends Mammal {
     @Override
-    public void print(){
+    public void print() {
         System.out.println("Я собака");
     }
 }
 
-class Reptile extends Animal{
+class Reptile extends Animal {
     @Override
-    public void print(){
+    public void print() {
         System.out.println("Рептилии");
     }
 }
